8b5ed4f80d5d15c9ea7061d4c0e5d5bbf8ea683f
Vishal Sanserwal <vsanse@outlook.com>
2021-08-30 22:18:53 +0530

    skills rearranged

--- src/components/Skills/skills_data.js
+++ src/components/Skills/skills_data.js
@@ -12,31 +12,35 @@ export const SkillsData = [
     title: "Redux",
   },
   {
-    tag: '<span class="iconify" data-icon="emojione-v1:lightning-mood"></span>',
-    title: "Lightning Web Components",
-  },
-  {
-    tag: '<span class="iconify" data-icon="logos:salesforce"></span>',
-    title: "Salesforce Commerce Cloud",
-  },
-  {
     tag: '<span class="iconify" data-icon="logos:javascript"></span>',
     title: "Javascript",
-  },
-  {
-    tag: '<span class="iconify" data-icon="logos:nodejs"></span>',
-    title: "Node JS",
-  },
-  {
-    tag: '<span class="iconify" data-icon="logos:bootstrap"></span>',
-    title: "Bootstrap",
   },
   {
     tag: '<span class="iconify" data-icon="logos:html-5"></span>',
     title: "HTML5",
   },
   {
+    tag: '<span class="iconify" data-icon="logos:css-3"></span>',
+    title: "CSS",
+  },
+  {
     tag: '<span class="iconify" data-icon="vscode-icons:file-type-sass"></span>',
     title: "SCSS",
+  },
+  {
+    tag: '<span class="iconify" data-icon="logos:bootstrap"></span>',
+    title: "Bootstrap",
+  },
+  {
+    tag: '<span class="iconify" data-icon="logos:nodejs"></span>',
+    title: "Node JS",
+  },
+  {
+    tag: '<span class="iconify" data-icon="emojione-v1:lightning-mood"></span>',
+    title: "Lightning Web Components",
+  },
+  {
+    tag: '<span class="iconify" data-icon="logos:salesforce"></span>',
+    title: "Salesforce Commerce Cloud",
   },
 ];
